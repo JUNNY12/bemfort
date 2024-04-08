a611def4d64e480b350067e4879a8bee5a53e8f4
X9 <david.agu@iisysgroup.com>
2024-04-08 14:35:08 +0100

    Cleaned Services

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,6 +1,8 @@
+import { Container } from "@/components/common/container/Container";
 import { FlexContainer } from "@/components/common/container/FlexContainer";
 import Jumbotron from "@/components/common/jumbotron/Jumbotron";
 import ExploreMore from "@/components/specific/Services/ExploreMore";
+import HowWeWork from "@/components/specific/Services/HowWeWork";
 import LOS from "@/components/specific/Services/LOS";
 
 export default function Services() {
@@ -9,7 +11,7 @@ export default function Services() {
       <Jumbotron jumboTitle={"Services"} jumboDesc={"Services"}/>
       <LOS/>
       <ExploreMore/>
-
+      <HowWeWork/>
     </div>
   )
 }
